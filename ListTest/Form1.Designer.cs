cf5434d6df075a421cf20958a32bf60d4b6e315a
sascha <tika1@gmx.de>
2015-04-28 21:10:31 +0200

    various index tests

--- ListTest/Form1.Designer.cs
+++ ListTest/Form1.Designer.cs
@@ -30,6 +30,9 @@
         {
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // comboBox1
@@ -42,19 +45,52 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(89, 174);
+            this.button1.Location = new System.Drawing.Point(10, 165);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "selectedItem";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(99, 165);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Text";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(185, 165);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(87, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "selectedValue";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(99, 194);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "selectedItem";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.comboBox1);
             this.Name = "Form1";
@@ -67,6 +103,9 @@
 
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- ListTest/Form1.cs
+++ ListTest/Form1.cs
@@ -17,8 +17,9 @@ namespace ListTest
     public partial class Form1 : Form
     {
         static Person p = new Person("Tim", "Taler");
+        static Person p2 = new Person("Jim", "Knopf");
 
-        static Person[] x = {p };
+        static Person[] x = {p,p2 };
 
         ArrayList flist = new ArrayList(x);
         public Form1()
@@ -38,7 +39,24 @@ namespace ListTest
         private void button1_Click(object sender, EventArgs e)
         {
             var person = this.comboBox1.SelectedItem as Person;
-            MessageBox.Show(p.firstName);
+            MessageBox.Show(p.ToString());
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this.comboBox1.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var person = this.comboBox1.SelectedValue as Person;
+            MessageBox.Show(p.ToString());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this.comboBox1.SelectedIndex.ToString());
         }
     }
 
@@ -56,6 +74,41 @@ namespace ListTest
         public override string ToString()
         {
             return this.firstName + " " + this.lastName;
+        }
+
+        public override bool Equals(System.Object p)
+        {
+            // If parameter is null return false:
+            if ((object)p == null)
+            {
+                return false;
+            }
+
+            var person = p as Person;
+
+            if (person == null)
+            {
+                return false;
+            }
+
+            
+
+            // Return true if the fields match:
+            return (firstName == person.firstName) && (lastName == person.lastName);
+        }
+
+        public bool Equals(Person p)
+        {
+            // If parameter is null return false:
+            if (p == null)
+            {
+                return false;
+            }
+
+            
+
+            // Return true if the fields match:
+            return (firstName == p.firstName) && (lastName == p.lastName);
         }
     }
 }
